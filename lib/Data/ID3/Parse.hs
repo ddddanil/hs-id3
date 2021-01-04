926683f30c718c812ed174a0347d4be80bfb640e
Danil Doroshin <ddddanil5555@gmail.com>
2021-01-04 12:54:55 +0300

    1E tag parser

--- lib/Data/ID3/Parse.hs
+++ lib/Data/ID3/Parse.hs
@@ -1,7 +1,6 @@
 module Data.ID3.Parse where
 
-import Protolude as P hiding (try) 
-import Data.Maybe (fromJust)
+import Protolude as P hiding (try)
 import qualified Data.Text as T
 import qualified Data.Text.Encoding as E
 import qualified Data.ByteString as BS
@@ -10,7 +9,7 @@ import Control.Lens.TH
 import Control.Monad.Combinators
 import Text.Megaparsec
 import Text.Megaparsec.Byte
-
+import Text.Megaparsec.Byte.Lexer
 import Data.ID3.Tag
 
 data ParserOpts = ParserOpts
@@ -43,12 +42,31 @@ parseID3v1Tag = do
   year <- T.pack <$> replicateM 4 (w8toC <$> digitChar)
   (comment, track) <- do
     comment <- parseTextField 28 <?> "comment"
-    choice [ do { zbyte <- try $ char 0;
+    choice [ do { _zbyte <- try $ char 0;
                   genre <- anySingle;
                   return (comment, Just genre)
           }, do { cont <- parseTextField 2 <?> "comment";
                   return (comment `T.append` cont, Nothing)
           }]
   genre <- anySingle 
-  return . fromJust $ mkID3v1Tag encode title artist album year comment track genre
+  return $ ID3v1Tag title artist album year comment track genre
 
+_parseID3v1ETag :: Parser ID3v1ETag
+_parseID3v1ETag = do
+  encode <- use encoder
+  _ <- string $ encode "TAG+"
+  title <- parseTextField 60 <?> "title"
+  artist <- parseTextField 60 <?> "artist"
+  album <- parseTextField 60 <?> "album"
+  speed <- anySingle
+  genre <- parseTextField 30 <?> "genre"
+  [start_time, end_time] <- replicateM 2 $ do
+    minutes <- decimal <?> "minutes"
+    guard $ minutes < 1000
+    _sep <- char . toEnum . fromEnum $ ':'
+    seconds <- decimal <?> "seconds"
+    guard $ seconds < 60
+    return $ (minutes, seconds)
+  return $ ID3v1ETag title artist album speed genre start_time end_time
+
+-- parseID3v1ETag = combine E + v1 tag
--- lib/Data/ID3/Tag.hs
+++ lib/Data/ID3/Tag.hs
@@ -1,13 +1,7 @@
-module Data.ID3.Tag (
-  ID3Ver(..)
-, ID3v1Tag
-, mkID3v1Tag
-) where
+module Data.ID3.Tag where
 
 import Protolude
-import Data.Char (isDigit)
 import qualified Data.Text as T
-import qualified Data.ByteString as BS
 import Control.Lens.TH
 
 -- | ID3 Supported versions
@@ -19,71 +13,32 @@ data ID3Ver
   | ID3v23 -- ^ v2.3
   | ID3v24 -- ^ v2.4
 
-textByteLength :: (T.Text -> BS.ByteString) -> T.Text -> Int
-textByteLength encoding = BS.length . encoding
-
 
 -- v1.0 and v1.1
 
 data ID3v1Tag = ID3v1Tag
-  { title :: T.Text
-  , artist :: T.Text 
-  , album :: T.Text
-  , year :: T.Text
-  , comment :: T.Text
-  , track :: Maybe Word8
-  , genre :: Word8 -- Do custom Type
+  { _title :: T.Text
+  , _artist :: T.Text 
+  , _album :: T.Text
+  , _year :: T.Text
+  , _comment :: T.Text
+  , _track :: Maybe Word8
+  , _genre :: Word8 -- Do custom Type
   }
   deriving (Eq, Show)
-
-mkID3v1Tag ::
-  (T.Text -> BS.ByteString) -- ^ encoding func
-  -> T.Text       -- ^ title
-  -> T.Text       -- ^ artist
-  -> T.Text       -- ^ album
-  -> T.Text       -- ^ year
-  -> T.Text       -- ^ comment
-  -> Maybe Word8  -- ^ track
-  -> Word8        -- ^ genre
-  -> Maybe ID3v1Tag
-mkID3v1Tag encoding title artist album year comment track genre
-  | textByteLength encoding title > 30 = Nothing
-  | textByteLength encoding artist > 30 = Nothing
-  | textByteLength encoding album > 30 = Nothing
-  | T.length year /= 4 || not (T.all isDigit year) = Nothing
-  | textByteLength encoding comment > 30 = Nothing
-  | isJust track && textByteLength encoding comment > 28 = Nothing
-  | otherwise = Just $ ID3v1Tag { .. }
+makeFields ''ID3v1Tag
 
 
 -- Enhanced tag
 
 data ID3v1ETag = ID3v1ETag
-  { title :: T.Text
-  , artist :: T.Text
-  , album :: T.Text
-  , speed :: Word8
-  , genre :: T.Text
-  , start_time :: (Int, Int)
-  , end_time :: (Int, Int)
+  { _title :: T.Text
+  , _artist :: T.Text
+  , _album :: T.Text
+  , _speed :: Word8
+  , _genre :: T.Text
+  , _start_time :: (Int, Int)
+  , _end_time :: (Int, Int)
   }
   deriving (Eq, Show)
-
-mkID3v1ETag ::
-  (T.Text -> BS.ByteString) -- ^ encoding func
-  -> T.Text       -- ^ title
-  -> T.Text       -- ^ artist
-  -> T.Text       -- ^ album
-  -> Word8        -- ^ speed
-  -> T.Text       -- ^ genre
-  -> (Int, Int)   -- ^ start time (mmm,ss)
-  -> (Int, Int)   -- ^ end time (mmm,ss)
-  -> Maybe ID3v1ETag
-mkID3v1ETag encoding title artist album speed genre start_time end_time
-  | textByteLength encoding title > 60 = Nothing
-  | textByteLength encoding artist > 60 = Nothing
-  | textByteLength encoding album > 60 = Nothing
-  | speed < 0 || speed > 4 = Nothing
-  | textByteLength encoding genre > 30 = Nothing
-  -- check times
-  | otherwise = Just $ ID3v1ETag { .. }
+makeFields ''ID3v1ETag
