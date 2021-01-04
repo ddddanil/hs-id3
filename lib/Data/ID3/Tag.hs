5b4ea77ff85655f7da948c51b7c462f5a319ab79
Danil Doroshin <ddddanil5555@gmail.com>
2021-01-04 12:15:17 +0300

    1E tag

--- lib/Data/ID3/Tag.hs
+++ lib/Data/ID3/Tag.hs
@@ -14,12 +14,16 @@ import Control.Lens.TH
 data ID3Ver
   = ID3v1  -- ^ v1 and v1.1
   | ID3v1E -- ^ unofficial Enhanced TAG
+  | ID3v12 -- ^ v1.2
   | ID3v22 -- ^ v2.2
   | ID3v23 -- ^ v2.3
   | ID3v24 -- ^ v2.4
 
 textByteLength :: (T.Text -> BS.ByteString) -> T.Text -> Int
 textByteLength encoding = BS.length . encoding
+
+
+-- v1.0 and v1.1
 
 data ID3v1Tag = ID3v1Tag
   { title :: T.Text
@@ -50,3 +54,36 @@ mkID3v1Tag encoding title artist album year comment track genre
   | textByteLength encoding comment > 30 = Nothing
   | isJust track && textByteLength encoding comment > 28 = Nothing
   | otherwise = Just $ ID3v1Tag { .. }
+
+
+-- Enhanced tag
+
+data ID3v1ETag = ID3v1ETag
+  { title :: T.Text
+  , artist :: T.Text
+  , album :: T.Text
+  , speed :: Word8
+  , genre :: T.Text
+  , start_time :: (Int, Int)
+  , end_time :: (Int, Int)
+  }
+  deriving (Eq, Show)
+
+mkID3v1ETag ::
+  (T.Text -> BS.ByteString) -- ^ encoding func
+  -> T.Text       -- ^ title
+  -> T.Text       -- ^ artist
+  -> T.Text       -- ^ album
+  -> Word8        -- ^ speed
+  -> T.Text       -- ^ genre
+  -> (Int, Int)   -- ^ start time (mmm,ss)
+  -> (Int, Int)   -- ^ end time (mmm,ss)
+  -> Maybe ID3v1ETag
+mkID3v1ETag encoding title artist album speed genre start_time end_time
+  | textByteLength encoding title > 60 = Nothing
+  | textByteLength encoding artist > 60 = Nothing
+  | textByteLength encoding album > 60 = Nothing
+  | speed < 0 || speed > 4 = Nothing
+  | textByteLength encoding genre > 30 = Nothing
+  -- check times
+  | otherwise = Just $ ID3v1ETag { .. }
